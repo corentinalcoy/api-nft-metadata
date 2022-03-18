2f0fb92d0c1d6918fd1c67ba118f4c755507533d
corentin <corentinalcoy@gmail.com>
2022-03-18 11:38:42 +0100

    - Add attributes to metadata

--- main.py
+++ main.py
@@ -8,46 +8,52 @@ metadata = [
         "description": "Beans Loyalty program",
         "image": "https://bnsre.s3.amazonaws.com/static-v3/connect/img/app/logo-full-liana.svg",
         "name": "Beans Loyalty",
+        "attributes": [{"key": "title", "value": "liana"}],
     },
     {
         "id": 1,
         "description": "Beans Referral program",
         "image": "https://bnsre.s3.amazonaws.com/static-v3/connect/img/app/logo-full-bamboo.svg",
         "name": "Beans Referral",
+        "attributes": [{"key": "title", "value": "bamboo"}],
     },
     {
         "id": 2,
         "description": "Beans Email program",
         "image": "https://bnsre.s3.amazonaws.com/static-v3/connect/img/app/logo-full-foxx.svg",
         "name": "Beans Email",
+        "attributes": [{"key": "title", "value": "foxx"}],
     },
     {
         "id": 3,
         "description": "Beans Popup program",
         "image": "https://bnsre.s3.amazonaws.com/static-v3/connect/img/app/logo-full-poppy.svg",
         "name": "Beans Loyalty",
+        "attributes": [{"key": "title", "value": "poppy"}],
     },
     {
         "id": 4,
         "description": "Beans Widget program",
         "image": "https://bnsre.s3.amazonaws.com/static-v3/connect/img/app/logo-full-snow.svg",
         "name": "Beans Widget",
+        "attributes": [{"key": "title", "value": "snow"}],
     },
     {
         "id": 6,
         "description": "Beans Social Connect Program ",
         "image": "https://bnsre.s3.amazonaws.com/static-v3/connect/img/app/logo-full-arrow.svg",
         "name": "Beans Social Connect",
+        "attributes": [{"key": "title", "value": "arrow"}],
     },
 ]
 
 
-@app.get("/trellix/metadata")
+@app.get("/trellix/token/metadata")
 async def list_metadata():
     return metadata
 
 
-@app.get("/trellix/metadata/{pk}")
+@app.get("/trellix/token/metadata/{pk}")
 async def read_metadata(pk: int):
     if pk > len(metadata) - 1:
         raise HTTPException(status_code=404, detail="Item not found")
