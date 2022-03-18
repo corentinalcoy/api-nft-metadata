c22bf3e3c57b61f66cfc11b3e846cc39415c0199
corentin <corentin@trellix.io>
2022-03-18 11:21:48 +0100

    - update metadata

--- main.py
+++ main.py
@@ -5,27 +5,49 @@ app = FastAPI()
 metadata = [
     {
         "id": 0,
-        "description": "Libanais Quatro Tag NFT",
-        "external_url": "https://unicore-beta.s3.amazonaws.com/media/tags/8f90cb4c1e5549e3884ff8f302cc9984.png",
-        "image": "https://unicore-beta.s3.amazonaws.com/media/tags/8f90cb4c1e5549e3884ff8f302cc9984.png",
-        "name": "Quatro Libanais Tag NFT",
+        "description": "Beans Loyalty program",
+        "image": "https://bnsre.s3.amazonaws.com/static-v3/connect/img/app/logo-full-liana.svg",
+        "name": "Beans Loyalty",
     },
     {
-        "id": 0,
-        "description": "Pizza Quatro Tag NFT",
-        "external_url": "https://unicore-beta.s3.amazonaws.com/media/tags/22ecf7bc506f4716b46fa470c5ff9c2f.png",
-        "image": "https://unicore-beta.s3.amazonaws.com/media/tags/22ecf7bc506f4716b46fa470c5ff9c2f.png",
-        "name": "Quatro Pizza Tag NFT",
+        "id": 1,
+        "description": "Beans Referral program",
+        "image": "https://bnsre.s3.amazonaws.com/static-v3/connect/img/app/logo-full-bamboo.svg",
+        "name": "Beans Referral",
+    },
+    {
+        "id": 2,
+        "description": "Beans Email program",
+        "image": "https://bnsre.s3.amazonaws.com/static-v3/connect/img/app/logo-full-foxx.svg",
+        "name": "Beans Email",
+    },
+    {
+        "id": 3,
+        "description": "Beans Popup program",
+        "image": "https://bnsre.s3.amazonaws.com/static-v3/connect/img/app/logo-full-poppy.svg",
+        "name": "Beans Loyalty",
+    },
+    {
+        "id": 4,
+        "description": "Beans Widget program",
+        "image": "https://bnsre.s3.amazonaws.com/static-v3/connect/img/app/logo-full-snow.svg",
+        "name": "Beans Widget",
+    },
+    {
+        "id": 6,
+        "description": "Beans Social Connect Program ",
+        "image": "https://bnsre.s3.amazonaws.com/static-v3/connect/img/app/logo-full-arrow.svg",
+        "name": "Beans Social Connect",
     },
 ]
 
 
-@app.get("/metadata")
+@app.get("/trellix/metadata")
 async def list_metadata():
     return metadata
 
 
-@app.get("/metadata/{pk}")
+@app.get("/trellix/metadata/{pk}")
 async def read_metadata(pk: int):
     if pk > len(metadata) - 1:
         raise HTTPException(status_code=404, detail="Item not found")
